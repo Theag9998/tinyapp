81cd91689857d0925401f3f8ed9eaecb0fb89dbe
Thea Ganden <wtp9998@hotmail.com>
2020-04-16 03:39:47 +0000

    ability to login if in the user database

--- express_server.js
+++ express_server.js
@@ -42,7 +42,7 @@ const users = {
   },
  "tommyboi": {
     id: "tommyboi", 
-    email: "thomas@example.com", 
+    email: "thomas1@example.com", 
     password: "5678"
   }
 }
@@ -66,7 +66,7 @@ app.get("/hello", (req, res) => {
   res.send("<html><body>Hello <b>World</b></body></html>\n");
 });
 
-//display the urls and the username
+//display the urls and the userId
 app.get("/urls", (req, res) => {
 	let templateVars = { userID: users[req.cookies['user_id']], urls: urlDatabase}
 	res.render("urls_index", templateVars)
@@ -76,6 +76,12 @@ app.get("/urls", (req, res) => {
 app.get("/register", (req, res) => {
 	let templateVars = { userID: users[req.cookies['user_id']]};
   res.render("register", templateVars);
+});
+
+//display the login page
+app.get("/login", (req, res) => {
+	let templateVars = { userID: users[req.cookies['user_id']]};
+  res.render("login", templateVars);
 });
 
 //display and create 
@@ -126,8 +132,32 @@ app.post("/urls/:shortURL", (req, res) => {
 
 //add end point to /login 
  app.post("/login", (req, res) => {
-	res.cookie("username", req.body['username'])
+	//get the info from body for email and password
+	const { email, password} = req.body;
+	console.log(req.body)
+	const currentId = idLookUp(email);
+	console.log(currentId)
+	const existingUser = users[currentId];
+	console.log(existingUser)
 
+	//if email or password are empty string send back 404 status
+	if (email === "" || password === "") {
+		res.send("Error 404 please enter email or password")
+	}
+	
+	//if someone enters an email and a matching password
+		if (emailLookUp(email) && existingUser.password === password) {
+		
+		//set a userID cookie containing the logged in ID
+			res.cookie("user_id", currentId);
+		//if email exists but the password does not match 
+		} else if (emailLookUp(email) && existingUser.password !== password) {
+			res.send("Error 403 password does not match")
+		//if email does not exist
+		} else {
+			res.send("Error 403 email cannot be found")
+		}
+	//after being logged in redirect to /urls
 	res.redirect("/urls")
 })
 
@@ -141,17 +171,18 @@ app.post("/logout", (req, res) => {
 
 //add end point to handle registration form data
 app.post("/register", (req, res) => {
+	//get the info from body for email and password
+	const {email, password} = req.body;
 	//if email or password are empty string send back 404 status
-	if (req.body['email'] === "" || req.body['password'] === "") {
-		res.send("Error 404 please enter email or password")
+	if (email === "" || password === "") {
+		res.send("Error 400 please enter email or password")
 	}
 
 	//if someone registers with an email that already exists in users return 404 status
-		if (emailLookUp(req.body['email']) === true) {
-		res.send("Error 404 email already exists")
+		if (emailLookUp(email)) {
+		res.send("Error 400 email already exists")
 		} else {
 			//update the users database with a unique id so new email and password are saved
-			const {email, password} = req.body;
 			users[uniqueID] = {'id': uniqueID, email, password}
 			//set a userID cookie containing the new generated ID
 			res.cookie("user_id", uniqueID);
@@ -167,8 +198,16 @@ const emailLookUp = function(email) {
 	for (key in users) {
 		if (email === users[key]['email']) {
 			return true
-		} else {
-			return false
-		}
+		} 
 	}
+	return false
+}
+
+const idLookUp = function(email) {
+	for (key in users) {
+		if (email === users[key]['email']) {
+			return users[key]['id']
+		} 
+	}
+	return false
 }
