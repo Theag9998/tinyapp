17f7d135cb24d3483313013491a3734a7365b2c8
Thea Ganden <wtp9998@hotmail.com>
2020-04-16 01:04:47 +0000

    compared user id to already existing users

--- express_server.js
+++ express_server.js
@@ -18,7 +18,9 @@ let chars = "0123456789abcdefghijklmnopqurstuvwxyzABCDEFGHIJKLMNOPQURSTUVWXYZ";
 return chars.substr(Math.floor(Math.random() * 62), 1);
 }
 //unique url is equal to the new random string
-let uniqueShortURL = generateRandomString()
+let uniqueShortURL = generateRandomString();
+//unique Id for a new registered user
+let uniqueID = generateRandomString();
 
 const bodyParser = require("body-parser");
 app.use(bodyParser.urlencoded({extended: true}));
@@ -33,12 +35,12 @@ const urlDatabase = {
 
 //an object which will be used to store and acess the users
 const users = { 
-  "userRandomID": {
+  "theabo": {
     id: "theabo", 
     email: "thea@example.com", 
     password: "12345"
   },
- "user2RandomID": {
+ "tommyboi": {
     id: "tommyboi", 
     email: "thomas@example.com", 
     password: "5678"
@@ -66,30 +68,30 @@ app.get("/hello", (req, res) => {
 
 //display the urls and the username
 app.get("/urls", (req, res) => {
-	let templateVars = {username: req.cookies["username"], urls: urlDatabase}
+	let templateVars = { userID: users[req.cookies['user_id']], urls: urlDatabase}
 	res.render("urls_index", templateVars)
 })
 
 //display the register page
 app.get("/register", (req, res) => {
-	let templateVars = { username: req.cookies["username"]};
+	let templateVars = { userID: users[req.cookies['user_id']]};
   res.render("register", templateVars);
 });
 
 //display and create 
 app.get("/urls/new", (req, res) => {
-	let templateVars = { username: req.cookies["username"]}
+	let templateVars = { userID: users[req.cookies['user_id']]}
   res.render("urls_new", templateVars);
 });
 
 //display the unique short url page
 app.get("/urls/:shortURL", (req, res) => {
   // /urls/:shortURL -> /urls/ABC123 -> req.params.shortURL === 'ABC123'
-  let templateVars = { username: req.cookies["username"], shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL] }; 
+  let templateVars = { userID: users[req.cookies['user_id']], shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL] }; 
 	res.render("urls_show", templateVars);
 });
 
-//
+//add a new short url to the urlDatabase when given a longURl
 app.post("/urls", (req, res) => {
 	// Log the POST request body of longUrlto the console
 	//console.log(req.body['longURL']);  
@@ -122,19 +124,51 @@ app.post("/urls/:shortURL", (req, res) => {
 	res.redirect(`/urls/${req.params.shortURL}`);
 })
 
-//add end point to /login and create username cookies
+//add end point to /login 
  app.post("/login", (req, res) => {
 	res.cookie("username", req.body['username'])
 
 	res.redirect("/urls")
 })
 
-//add end point to /logout and remove the username cookies
+//add end point to /logout and remove the user_id cookies
 app.post("/logout", (req, res) => {
 
-	res.clearCookie("username")
+	res.clearCookie("user_id")
 
 	res.redirect("/urls")
 })
 
 //add end point to handle registration form data
+app.post("/register", (req, res) => {
+	//if email or password are empty string send back 404 status
+	if (req.body['email'] === "" || req.body['password'] === "") {
+		res.send("Error 404 please enter email or password")
+	}
+
+	//if someone registers with an email that already exists in users return 404 status
+		if (emailLookUp(req.body['email']) === true) {
+		res.send("Error 404 email already exists")
+		} else {
+			//update the users database with a unique id so new email and password are saved
+			const {email, password} = req.body;
+			users[uniqueID] = {'id': uniqueID, email, password}
+			//set a userID cookie containing the new generated ID
+			res.cookie("user_id", uniqueID);
+		}
+	//after being registered redirect to /urls
+	res.redirect("/urls")
+})
+
+
+//email look up function
+const emailLookUp = function(email) {
+	//search through users to see if email entered is already registered
+	for (key in users) {
+		if (email === users[key]['email']) {
+			return true
+		} else {
+			return false
+		}
+	}
+}
